61cdf25508343c9238bff69c0b159ff68cb66771
Harsh Wardhan <harshwardhan847@gmail.com>
2025-08-11 18:28:26 +0530

    corrected proxy url

--- src/fal/index.ts
+++ src/fal/index.ts
@@ -3,7 +3,7 @@ import { fal } from "@fal-ai/client";
 const falConfig = () =>
   fal.config({
     credentials: process.env.FAL_KEY,
-    proxyUrl: "/api/falai/proxy",
+    proxyUrl: "/api/fal/proxy",
   });
 
 export default falConfig;
