2b436a845ea83115abd6a1f3d33ca974ad9e7f3a
Harsh Wardhan <harshwardhan847@gmail.com>
2025-08-11 18:27:07 +0530

    added proxy logic

--- src/fal/index.ts
+++ src/fal/index.ts
@@ -3,6 +3,7 @@ import { fal } from "@fal-ai/client";
 const falConfig = () =>
   fal.config({
     credentials: process.env.FAL_KEY,
+    proxyUrl: "/api/falai/proxy",
   });
 
 export default falConfig;
