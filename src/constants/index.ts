fc6f8522460c4b6461070d0a96994ecbf1f95dba
Harsh Wardhan <harshwardhan847@gmail.com>
2025-08-21 10:21:24 +0530

    updated contact details

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const calendlyLink =
   "https://calendly.com/signaturestudio/snapfuse-siscussion";
 
-export const phoneNumber = "+91-9876543210";
-export const whatsappNumber = "+919876543210"; // in international format, no + or dashes
-export const email = "your@email.com";
+export const phoneNumber = "+91-7988543957";
+export const whatsappNumber = "+917988543957"; // in international format, no + or dashes
+export const email = "harshwardhan847@gmail.com";
